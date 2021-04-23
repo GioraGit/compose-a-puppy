938ebbb8b37a15de2345e4105da2c84a74977f4e
Giora <giora.shevach@climacell.co>
2021-04-23 16:53:33 +0300

    Create a card composable

---
+++ app/src/main/java/com/example/androiddevchallenge/composables/ACard.kt
@@ -0,0 +1,78 @@
+package com.example.androiddevchallenge.composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.androiddevchallenge.ui.theme.MyTheme
+
+@Composable
+fun ACard(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Card(
+        modifier = modifier,
+        backgroundColor = MaterialTheme.colors.surface,
+        shape = RoundedCornerShape(8.dp),
+        elevation = 1.dp,
+        content = content
+    )
+}
+
+@Preview(name = "Light Theme", backgroundColor = 0xF2F2F7, showBackground = true)
+@Composable
+fun LightPreview() {
+    MyTheme(darkTheme = false) {
+        ACard(
+            modifier = Modifier
+                .padding(16.dp)
+                .width(100.dp)
+                .height(150.dp)
+        ) {
+            Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(
+                    text = "This is a very long text",
+                    fontSize = 10.sp,
+                    fontFamily = FontFamily.Monospace,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+}
+
+@Preview(name = "Dark Theme", backgroundColor = 0x121212, showBackground = true)
+@Composable
+fun DarkPreview() {
+    MyTheme(darkTheme = true) {
+        ACard(
+            modifier = Modifier
+                .padding(16.dp)
+                .width(100.dp)
+                .height(150.dp)
+        ) {
+            Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(
+                    text = "This is a very long text",
+                    fontSize = 10.sp,
+                    fontFamily = FontFamily.Monospace,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+}
